846b11ce02b35293a9587f6a6aae5bb243ecfc6d
drr382054-art <drr382054@gmail.com>
2026-03-09 05:28:34 +0300

    Fix import path for DhikrCard component

--- src/AdhkarDetailPage.tsx
+++ src/AdhkarDetailPage.tsx
@@ -1,7 +1,7 @@
 import { useState, useMemo } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
-import DhikrCard from '@/components/DhikrCard';
+import DhikrCard from './DhikrCard';
 import { useGhars } from '@/stores/useGharsStore';
 import { morningAdhkar, eveningAdhkar, afterPrayerAdhkar, sleepAdhkar } from '@/data/adhkarData';
 
